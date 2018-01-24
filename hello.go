4e32e17c38646403459bf0ac429ef4f1f476d2ea
ErlendT <32453298+ErlendT@users.noreply.github.com>
2018-01-24 11:59:36 +0100

    fix

--- hello.go
+++ hello.go
@@ -5,12 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, ??")
 	fmt.Println("Erlend")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 	fmt.Println("Vegar")
->>>>>>> 970228bf558ea53bbc3fe632a256b9535420103f
-=======
-	fmt.Println("Vegar")
->>>>>>> 970228bf558ea53bbc3fe632a256b9535420103f
 }
