82289509a3e5252def17987b3b90fe02a5e2b103
Vegar <vegarr14@uia.no>
2018-03-05 13:56:45 +0100

    oppgave2v3

--- Oblig2/src/oppgaver/oppgave2.go
+++ Oblig2/src/oppgaver/oppgave2.go
@@ -9,33 +9,37 @@ import (
 )
 
 func main() {
-
-
-
-
-
+	fmt.Println("Information about text.txt")
 	file, _ := os.Open("text.txt")
 	fileScanner := bufio.NewScanner(file)
 	lineCount := 0
 	for fileScanner.Scan() {
 	    lineCount++
 	}
+	fmt.Println("Number of lines in file:", lineCount)
 
-	fmt.Println("number of lines:", lineCount)
-
-	_, err := ioutil.ReadFile("text.txt")
+	var a [256]int
+	content, err := ioutil.ReadFile("text.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-
-
-
-
-
-
-
-
-
-	fmt.Printf("Ferdig")
+	for i := 0; i < len(content); i++ {
+		a[content[i]]++
+	}
+	fmt.Println("Most common runes")
+	for i := 0; i < 5; i++ {
+		antall := 0
+		tegn := 0
+		for i := 0; i < len(a); i++ {
+			if antall < a[i] {
+				antall = a[i]
+				tegn = i
+			}
+		}
+		fmt.Println(i+1)
+		fmt.Printf("Rune: ",)
+		fmt.Printf("%q", tegn)
+		fmt.Println(", Counts: ",antall)
+		a[tegn] = 0
+	}
 }
