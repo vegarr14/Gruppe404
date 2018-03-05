1ace7fe4c2215df7398faaa0f84639167ec01e8e
Vegar <vegarr14@uia.no>
2018-03-05 11:13:19 +0100

    1.a.v2

--- Oblig2/src/oppgaver/fileinfo.go
+++ Oblig2/src/oppgaver/fileinfo.go
@@ -14,4 +14,25 @@ func main () {
   }
   fmt.Println("Information about file", fi.Name())
   fmt.Println("Size:", fi.Size(), "bytes." , fi.Size()/1024, "KB.", fi.Size()/1024/1024, "MB.", fi.Size()/1024/1024/1024, "GB.")
+  if (fi.Mode().IsDir()==false) {
+    fmt.Println("Is not a diretory.")
+  } else {
+    fmt.Println("Is a directory.")
+  }
+  if (fi.Mode().IsRegular()==false) {
+    fmt.Println("Is not a regular file.")
+  } else {
+    fmt.Println("Is a regular file.")
+  }
+  fmt.Println("Has Unix permission bits:", fi.Mode())
+
+  ft := fi.Mode();
+  fmt.Println(ft&os.ModeAppend)
+  fmt.Println(ft&os.ModeDevice)
+  fmt.Println(ft&os.ModeCharDevice)
+  fmt.Println(ft&os.ModeSymlink)
+
+
+
+
 }
