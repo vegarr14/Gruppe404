7b7889704ca9fd354600b55dac328382073b7b62
Vegar <vegarr14@uia.no>
2018-03-05 12:16:36 +0100

    oppgave2v2

--- Oblig2/src/oppgaver/oppgave2.go
+++ Oblig2/src/oppgaver/oppgave2.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"bufio"
 )
 
 func main() {
-	content, err := ioutil.ReadFile("text.txt")
+
+
+
+
+
+	file, _ := os.Open("text.txt")
+	fileScanner := bufio.NewScanner(file)
+	lineCount := 0
+	for fileScanner.Scan() {
+	    lineCount++
+	}
+
+	fmt.Println("number of lines:", lineCount)
+
+	_, err := ioutil.ReadFile("text.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("File contents: %s", content)
+
+
+
+
+
+
+
+
+
+	fmt.Printf("Ferdig")
 }
