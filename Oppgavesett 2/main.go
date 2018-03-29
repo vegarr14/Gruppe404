7280435bbab08d1cc41944b98cc726be21a0aeb7
Vegar <vegarr14@uia.no>
2018-03-29 14:22:06 +0200

    oppgavesett 2

---
+++ Oppgavesett 2/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"math/rand"
+	"log"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/event/touch"
+	"golang.org/x/mobile/exp/app/debug"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/gl"
+)
+
+var (
+	images   *glutil.Images
+	fps      *debug.FPS
+	program  gl.Program
+
+	green  float32
+	number1 float32
+	number2 float32
+	number3 float32
+)
+
+func main() {
+	app.Main(func(a app.App) {
+		var glctx gl.Context
+		var sz size.Event
+		for e := range a.Events() {
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					glctx, _ = e.DrawContext.(gl.Context)
+					onStart(glctx)
+					a.Send(paint.Event{})
+				case lifecycle.CrossOff:
+					onStop(glctx)
+					glctx = nil
+				}
+			case size.Event:
+				sz = e
+			case paint.Event:
+				if glctx == nil || e.External {
+					continue
+				}
+
+				onPaint(glctx, sz)
+				a.Publish()
+				a.Send(paint.Event{})
+			case touch.Event:
+				t := e.Type
+				if t == touch.TypeBegin {
+					number1 = rand.Float32()
+					number2 = rand.Float32()
+					number3 = rand.Float32()
+				}
+			}
+		}
+	})
+}
+
+func onStart(glctx gl.Context) {
+	var err error
+	program, err = glutil.CreateProgram(glctx, vertexShader, fragmentShader)
+	if err != nil {
+		log.Printf("error creating GL program: %v", err)
+		return
+	}
+
+	images = glutil.NewImages(glctx)
+	fps = debug.NewFPS(images)
+}
+
+func onStop(glctx gl.Context) {
+	glctx.DeleteProgram(program)
+	fps.Release()
+	images.Release()
+}
+
+func onPaint(glctx gl.Context, sz size.Event) {
+	glctx.ClearColor(number1, number2, number3, 1)
+	glctx.Clear(gl.COLOR_BUFFER_BIT)
+
+	glctx.UseProgram(program)
+	fps.Draw(sz)
+}
+
+const vertexShader = `#version 100
+uniform vec2 offset;
+
+attribute vec4 position;
+void main() {
+	// offset comes in with x/y values between 0 and 1.
+	// position bounds are -1 to 1.
+	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
+	gl_Position = position + offset4;
+}`
+
+const fragmentShader = `#version 100
+precision mediump float;
+uniform vec4 color;
+void main() {
+	gl_FragColor = color;
+}`
