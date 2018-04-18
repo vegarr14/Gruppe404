1611adac2751452f16d6e925cefd43f6fd5014a6
JosefSkar <35764643+JosefSkar@users.noreply.github.com>
2018-04-18 14:02:46 +0200

    jaja
    push

--- oblig 3/oppgave 1 og 2/webserver.go
+++ oblig 3/oppgave 1 og 2/webserver.go
@@ -47,13 +47,29 @@ func json1(w http.ResponseWriter, req *http.Request) {
      io.WriteString(w, m[i].Klokkeslett + " / " + m[i].Sted + " / " + m[i].Antall_ledige_plasser + "\n")
    }
   }
+  func json3(w http.ResponseWriter, req *http.Request) {
+    type Variabler struct {
+      Art, Tjeneste_tekst, Virksomhetsomraade, Art_tekst, Tjeneste_nr, Inntekt_utgift string
+   }
+    var m []Variabler
+    link := ("https://hotell.difi.no/api/json/stavanger/budsjettforslag2016")
+    err := json.Unmarshal(getJson(link), &m)
+    if err != nil {
+      fmt.Println("error:", err)
+   }
+    io.WriteString(w, "Viser budsjettforslag for 2016 i Stavanger\n")
+    for i := 0 ; i < len(m) ; i++ {
+      io.WriteString(w, m[i].Art + " / " + m[i].Tjeneste_tekst + " / " + m[i].Virksomhetsomraade + " / " + m[i].Virksomhetsomraade + " / " + m[i].Art_tekst + " / " + m[i].Tjeneste_nr + " / " + m[i].Inntekt_utgift + "\n")
+   }
+  }
 
 
 func main(){
   http.HandleFunc("/", hello)
   http.HandleFunc("/1", json1)
   http.HandleFunc("/2", json2)
-//  http.HandleFunc("/3", json3)
+  http.HandleFunc("/3", json3)
+  //  http.HandleFunc("/4", json4)
 //  http.HandleFunc("/5", json5)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
  }
