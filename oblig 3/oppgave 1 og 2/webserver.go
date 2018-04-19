5a0a936adee131395939afb6511a19682ab65b75
ErlendT <32453298+ErlendT@users.noreply.github.com>
2018-04-19 14:12:18 +0200

    Fikset html template for 3,4. La til CSS
    Ska ta igjen vegar på commits/linjer med  kode

--- oblig 3/oppgave 1 og 2/webserver.go
+++ oblig 3/oppgave 1 og 2/webserver.go
@@ -39,12 +39,8 @@ func json1(w http.ResponseWriter, req *http.Request) {
     Linje2: "Sted / Plast / glass&metall / tekstil&sko",
     Array: m,
   }
+
   tmpl.Execute(w, data)
-  //io.WriteString(w, "Miljøstasjoner i Stavanger\n")
-  //io.WriteString(w, "Sted / Tar plast / tar glass&metall / tar tekstil&sko\n")
-  //for i := 0 ; i < len(m) ; i++ {
-  //  io.WriteString(w, m[i].Navn + " / " + m[i].Plast + " / " + m[i].Glass_metall + " / " + m[i].Tekstil_sko + "\n")
-  //}
  }
 
  func json2(w http.ResponseWriter, req *http.Request) {
@@ -73,41 +69,60 @@ func json1(w http.ResponseWriter, req *http.Request) {
    }
      tmpl.Execute(w, data)
 
-   //io.WriteString(w, "navn, lokalisering, ledige plasser \n")
-   //for i := 0 ; i < len(m) ; i++ {
-  //   io.WriteString(w, m[i].Klokkeslett + " / " + m[i].Sted + " / " + m[i].Antall_ledige_plasser + "\n")
-   //}
   }
   func json3(w http.ResponseWriter, req *http.Request) {
     type Variabler struct {
-      Art, Tjeneste_tekst, Virksomhetsomraade, Art_tekst, Tjeneste_nr, Inntekt_utgift string
+      Virksomhetsomraade, Art_tekst, Inntekt_utgift string
    }
+
+   type htmlStruct struct {
+     PageTitle string
+     Linje2 string
+     Array []Variabler
+   }
+
     var m []Variabler
+    tmpl := template.Must(template.ParseFiles("layout3.html"))
     link := ("https://hotell.difi.no/api/json/stavanger/budsjettforslag2016")
     err := json.Unmarshal(getJson(link), &m)
     if err != nil {
       fmt.Println("error:", err)
    }
-    io.WriteString(w, "Viser budsjettforslag for 2016 i Stavanger\n")
-    for i := 0 ; i < len(m) ; i++ {
-      io.WriteString(w, m[i].Art + " / " + m[i].Tjeneste_tekst + " / " + m[i].Virksomhetsomraade + " / " + m[i].Virksomhetsomraade + " / " + m[i].Art_tekst + " / " + m[i].Tjeneste_nr + " / " + m[i].Inntekt_utgift + "\n")
+
+   data := htmlStruct{
+     PageTitle: "Budsjettforslag for Stavanger, 2016",
+     Linje2: "Virksomhetsområde / Beskrivelse / Inntekt eller utgift",
+     Array: m,
    }
+
+   tmpl.Execute(w, data)
   }
 
   func json4(w http.ResponseWriter, req *http.Request) {
     type Variabler struct {
-      Nord, Barnehagens_navn, Lengdegrad, Oest, Adresse, Breddegrad string
+      Barnehagens_navn, Adresse string
+   }
+
+   type htmlStruct struct {
+     PageTitle string
+     Linje2 string
+     Array []Variabler
    }
     var m []Variabler
+    tmpl := template.Must(template.ParseFiles("layout4.html"))
     link := ("https://hotell.difi.no/api/json/stavanger/barnehager")
     err := json.Unmarshal(getJson(link), &m)
     if err != nil {
       fmt.Println("error:", err)
    }
-    io.WriteString(w, "Viser budsjettforslag for 2016 i Stavanger\n")
-    for i := 0 ; i < len(m) ; i++ {
-      io.WriteString(w, m[i].Nord + " / " + m[i].Barnehagens_navn + " / " + m[i].Lengdegrad + " / " + m[i].Oest + " / " + m[i].Adresse + " / " + m[i].Breddegrad + "\n")
+
+   data := htmlStruct{
+     PageTitle: "Barnehager i Stavanger",
+     Linje2: "Navn / Addresse",
+     Array: m,
    }
+
+   tmpl.Execute(w, data)
   }
 
   func json5(w http.ResponseWriter, req *http.Request) {
@@ -133,8 +148,12 @@ func json1(w http.ResponseWriter, req *http.Request) {
       Linje2: "Navn / Addresse",
       Array: m,
     }
-    tmpl.Execute(w, data)
 
+    tmpl.Execute(w, data)
+   }
+
+   func css(w http.ResponseWriter, req *http.Request){
+     http.ServeFile(w, req, "style.css")
    }
 
 func main(){
@@ -142,8 +161,10 @@ func main(){
   http.HandleFunc("/1", json1)
   http.HandleFunc("/2", json2)
   http.HandleFunc("/3", json3)
-  //  http.HandleFunc("/4", json4)
+  http.HandleFunc("/4", json4)
   http.HandleFunc("/5", json5)
+  //Henter stylesheet
+  http.HandleFunc("/style.css", css)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
  }
 
