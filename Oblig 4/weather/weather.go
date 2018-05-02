3579ea3dcea7695801c83d045bc4ceb3ebfced56
Vegar <vegarr14@uia.no>
2018-05-02 13:44:11 +0200

    forecast

--- Oblig 4/weather/weather.go
+++ Oblig 4/weather/weather.go
@@ -6,6 +6,13 @@ import (
   "io/ioutil"
   "encoding/json"
 )
+
+func CheckError(err error) {
+  if err != nil {
+    fmt.Println("error:", err)
+  }
+}
+
 type Feilmelding struct {
   Cod string
   Message string
@@ -65,8 +72,67 @@ func CurrentWeather(location string) (Feilmelding, CWStruct) {
   return feil,m
 }
 
-func CheckError(err error) {
-  if err != nil {
-    fmt.Println("error:", err)
-  }
+type FStruct struct {
+	Cod     string  `json:"cod"`
+	Message float64 `json:"message"`
+	Cnt     int     `json:"cnt"`
+	List    []struct {
+		Dt   int `json:"dt"`
+		Main struct {
+			Temp      float64 `json:"temp"`
+			TempMin   float64 `json:"temp_min"`
+			TempMax   float64 `json:"temp_max"`
+			Pressure  float64 `json:"pressure"`
+			SeaLevel  float64 `json:"sea_level"`
+			GrndLevel float64 `json:"grnd_level"`
+			Humidity  int     `json:"humidity"`
+			TempKf    float64 `json:"temp_kf"`
+		} `json:"main"`
+		Weather []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+			Deg   float64 `json:"deg"`
+		} `json:"wind"`
+		Rain struct {
+			ThreeH float64 `json:"3h"`
+		} `json:"rain"`
+		Sys struct {
+			Pod string `json:"pod"`
+		} `json:"sys"`
+		DtTxt string `json:"dt_txt"`
+		Snow  struct {
+			ThreeH float64 `json:"3h"`
+		} `json:"snow,omitempty"`
+	} `json:"list"`
+	City struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Coord struct {
+			Lat float64 `json:"lat"`
+			Lon float64 `json:"lon"`
+		} `json:"coord"`
+		Country    string `json:"country"`
+		Population int    `json:"population"`
+	} `json:"city"`
+}
+
+func Forecast(location string) (Feilmelding, FStruct) {
+  var m FStruct
+  var feil Feilmelding
+  link := "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=metric&appid=b1bf40e9707aee87cf7f39cd96df39b1"
+  resp, err := http.Get(link)
+  CheckError(err)
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  _ = json.Unmarshal(body, &feil)
+  _ = json.Unmarshal(body, &m)
+  return feil,m
 }
