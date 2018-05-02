9d4747798aca04b293379afbfdafdef8ead8c727
raglefanten <35764619+raglefanten@users.noreply.github.com>
2018-05-02 15:08:52 +0200

    weather types

--- Oblig 4/weather/responsecreator/responsecreator.go
+++ Oblig 4/weather/responsecreator/responsecreator.go
@@ -8,6 +8,20 @@ func Getresponse(input int) (data) {
   var s string
   if 200<= input && input<=232  {
     s ="thunder"
+  }else if 300<= input && input<= 321 {
+    s ="fo' drizzle muh nizzle"
+  }else if 500<= input && input <= 531 {
+    s= "rain"
+  }else if 600<= input && input <= 622 {
+    s= "you know nothing (jon)snow"
+  }else if 701<= input && input <= 781 {
+    s= "there is something bad happening"
+  }else if == 800 input {
+    s= "all clear"
+  }else if 801<= input && input <= 804 {
+    s= "clouds"
   }
+
   return s
+
   }
