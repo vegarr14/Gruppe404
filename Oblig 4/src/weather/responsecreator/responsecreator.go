a862acad7127426e840173618a2d450f5f16e081
ErlendT <32453298+ErlendT@users.noreply.github.com>
2018-05-10 14:57:08 +0200

    added some message text and source openweater

--- Oblig 4/src/weather/responsecreator/responsecreator.go
+++ Oblig 4/src/weather/responsecreator/responsecreator.go
@@ -16,16 +16,16 @@ func Getresponse(input int) (string, string) {
     s ="Fo' drizzle muh nizzle"
     imgPath = "weather/icons/solmedlittskyer.png"
   }else if 500<= input && input <= 531 {
-    s= "I'll be crying in the rain"
+    s= "You might want to take an umbrella with you because\nI'll be crying in the rain"
     imgPath = "weather/icons/regn.png"
   }else if 600<= input && input <= 622 {
-    s= "Let it snow, let it snow, let it snow"
+    s= "You might need some gloves because the clouds decided to\nLet it snow, let it snow, let it snow"
     imgPath = "weather/icons/sno.png"
   }else if 701<= input && input <= 781 {
     s= "There is something bad happening here!\n What it is ain't exactly clear!"
     imgPath = "weather/icons/Tornado.png"
   }else if 800 == input {
-    s= "You are my sunshine, my only sunshine!"
+    s= "Bring your sunglasses with you because\nYou are my sunshine, my only sunshine!"
     imgPath = "weather/icons/sol.png"
   }else if 801<= input && input <= 804 {
     s= "Take me to the clouds above"
@@ -42,15 +42,15 @@ func Time(sunrise int64, sunset int64, unixnow int64) (string) {
   if unixnow < sunrise {
     d,err = time.ParseDuration(strconv.FormatInt(sunrise - unixnow, 10) + "s")
     CheckError(err)
-    t = "sunrise in " + d.String()
+    t = "Sunrise is in " + d.String()
   } else if unixnow < sunset {
     d,err = time.ParseDuration(strconv.FormatInt(sunset - unixnow, 10) + "s")
     CheckError(err)
-    t = "sunset in " + d.String()
+    t = "Sunset is in " + d.String()
   } else if unixnow > sunset {
     d,err = time.ParseDuration(strconv.FormatInt(unixnow - sunset, 10) + "s")
     CheckError(err)
-    t = "sunset was " + d.String() + " ago"
+    t = "Sunset was " + d.String() + " ago"
   }
   return t
 }
