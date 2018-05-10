c867164d072b1814afd315966e02462d6f23a4ac
Vegar <vegarr14@uia.no>
2018-05-10 14:31:11 +0200

    testing 1

--- Oblig 4/src/weather/responsecreator/responsecreator_test.go
+++ Oblig 4/src/weather/responsecreator/responsecreator_test.go
@@ -1,12 +1,24 @@
-package responsecreator_test
+package responsecreator
 
 import (
   "testing"
-  "responsecreator"
-  "fmt"
+  "strings"
+  "time"
+  "weather/responsecreator"
 )
 
-func TestSomething(t *testing.T) {
-  meme := responsecreator.Time(0, 10000, 500)
-  fmt.Println(meme)
+func TestTimePositive(t *testing.T) {
+  test := responsecreator.Time(0, 10000, 500)
+  svar := "sunset in 2h38m20s"
+  if strings.Compare(test, svar) != 0 {
+    t.Errorf("test failed. got %d, want %d.", test, svar)
+  }
+}
+
+func TestTimeNegative(t *testing.T) {
+  test := responsecreator.Time(1525940000, 1525970000, time.Now().Unix())
+  svar := "sunset in 4h4m48s"
+  if strings.Compare(test, svar) != 0 {
+    t.Errorf("test failed. got %d, want %d.", test, svar)
+  }
 }
